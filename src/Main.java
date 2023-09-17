da661fa4319b84971f6c2c0aa562ff12d500179b
captain-dj <whardig10@gmail.com>
2023-09-17 19:25:01 -0400

    V1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello here are each of your seasonal maintaints cost");
+        System.out.println("Winter = 2000 Spring = 600 summer = 1000 Fall = 500");
+
+        double doubleOperandA = 2000;
+        double doubleOperandB = 600;
+        double doubleOperandC = 1000;
+        double doubleOperandD = 500;
+        double doubleSum = 0;
+
+        doubleSum = doubleOperandA + doubleOperandB + doubleOperandC + doubleOperandD;
+        System.out.println("Your maintainance cost after the hole year is "+ doubleSum +" ");
     }
 }
